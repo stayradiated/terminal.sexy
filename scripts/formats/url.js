ded60f69f5776601c4cb50c24c45d075f0e7fccf
George Czabania <george@czabania.com>
2014-04-15 10:55:57 +1200

    Fix formats/url and update tests

--- scripts/formats/url.js
+++ scripts/formats/url.js
@@ -1,28 +1,36 @@
 var base64 = require('urlsafe-base64');
 
+
 tinycolor.prototype.toRgbArray = function () {
   var rgb = this.toRgb();
   return [ rgb.r, rgb.g, rgb.b ];
 };
 
+
+var ORDER = [
+  'background', 'foreground',
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+];
+
+
 module.exports = {
 
   /*
    * url.import
+   *
+   * Convert a 76 byte string into a collection of colors
+   *
+   * - input (string)
+   * > output (object)
    */
 
   import: function (input) {
     var output = {};
     var buffer = base64.decode(input);
 
-    for (var i = 0, len = buffer.length; i < len; i += 4) {
-      var id = buffer[i];
-      switch (id) {
-        case 102: id = 'foreground'; break;
-        case 98: id = 'background'; break;
-      }
-      if (id > 15) continue;
-      var color = buffer.slice(i+1, i+4).toString('hex');
+    for (var i = 0, len = ORDER.length; i < len; i++) {
+      var id = ORDER[i];
+      var color = buffer.slice(i * 3, i * 3 + 3).toString('hex');
       output[id] = tinycolor(color);
     }
 
@@ -33,26 +41,16 @@ module.exports = {
   /*
    * url.export
    *
-   * Convert into a base64 string to save space
+   * Convert into a 76 byte url-base64 string to save space
    *
    * - input (object)
    * > output (string)
    */
 
   export: function (input) {
-
-    // format [<id>, <red>, <green>, <blue>, <id>, <red>, ...]
-    // [0, 10, 10, 10, 1, 20, 20, 20, 2, 30, 30, 30, ...]
-    // foreground id = 98
-    // background id = 102
-
-    var array = _.chain(input)
-      .map(function (color, name) {
-        var id = isNaN(parseInt(name)) ? name.charCodeAt(0) : parseInt(name);
-        return [id, color.toRgbArray()];
-      })
-      .flatten()
-      .value();
+    var array = _.chain(ORDER).map(function (name) {
+      return input[name].toRgbArray();
+    }).flatten().value();
 
     var buffer = new Buffer(array);
     return base64.encode(buffer);
--- test/formats/css.js
+++ test/formats/css.js
@@ -35,38 +35,38 @@ describe('formats/css', function () {
       assert.equal(output,
       '.bg-bg { background: #000000; }\n'+
       '.fg-fg { color: #ffffff; }\n'+
-      '.bg-0 { background: #000000; }\n'+
       '.fg-0 { color: #000000; }\n'+
-      '.bg-1 { background: #111111; }\n'+
+      '.bg-0 { background: #000000; }\n'+
       '.fg-1 { color: #111111; }\n'+
-      '.bg-2 { background: #222222; }\n'+
+      '.bg-1 { background: #111111; }\n'+
       '.fg-2 { color: #222222; }\n'+
-      '.bg-3 { background: #333333; }\n'+
+      '.bg-2 { background: #222222; }\n'+
       '.fg-3 { color: #333333; }\n'+
-      '.bg-4 { background: #444444; }\n'+
+      '.bg-3 { background: #333333; }\n'+
       '.fg-4 { color: #444444; }\n'+
-      '.bg-5 { background: #555555; }\n'+
+      '.bg-4 { background: #444444; }\n'+
       '.fg-5 { color: #555555; }\n'+
-      '.bg-6 { background: #666666; }\n'+
+      '.bg-5 { background: #555555; }\n'+
       '.fg-6 { color: #666666; }\n'+
-      '.bg-7 { background: #777777; }\n'+
+      '.bg-6 { background: #666666; }\n'+
       '.fg-7 { color: #777777; }\n'+
+      '.bg-7 { background: #777777; }\n'+
+      '.bold .fg-0, .fg-8 { color: #888888; }\n'+
       '.bg-8 { background: #888888; }\n'+
-      '.fg-8 { color: #888888; }\n'+
+      '.bold .fg-1, .fg-9 { color: #999999; }\n'+
       '.bg-9 { background: #999999; }\n'+
-      '.fg-9 { color: #999999; }\n'+
+      '.bold .fg-2, .fg-10 { color: #aaaaaa; }\n'+
       '.bg-10 { background: #aaaaaa; }\n'+
-      '.fg-10 { color: #aaaaaa; }\n'+
+      '.bold .fg-3, .fg-11 { color: #bbbbbb; }\n'+
       '.bg-11 { background: #bbbbbb; }\n'+
-      '.fg-11 { color: #bbbbbb; }\n'+
+      '.bold .fg-4, .fg-12 { color: #cccccc; }\n'+
       '.bg-12 { background: #cccccc; }\n'+
-      '.fg-12 { color: #cccccc; }\n'+
+      '.bold .fg-5, .fg-13 { color: #dddddd; }\n'+
       '.bg-13 { background: #dddddd; }\n'+
-      '.fg-13 { color: #dddddd; }\n'+
+      '.bold .fg-6, .fg-14 { color: #eeeeee; }\n'+
       '.bg-14 { background: #eeeeee; }\n'+
-      '.fg-14 { color: #eeeeee; }\n'+
-      '.bg-15 { background: #ffffff; }\n'+
-      '.fg-15 { color: #ffffff; }\n'
+      '.bold .fg-7, .fg-15 { color: #ffffff; }\n'+
+      '.bg-15 { background: #ffffff; }\n'
       );
 
     });
