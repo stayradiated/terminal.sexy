f63bdc4b45aa9c3001314014940b1f442237c2e9
George Czabania <george@czabania.com>
2014-08-26 16:25:26 +1200

    Use Ranger 0.4.x with async loaded templates index

--- lib/components/schemeBrowser.react.jsx
+++ lib/components/schemeBrowser.react.jsx
@@ -3,26 +3,14 @@
 var React = require('react');
 var Ranger = require('react-ranger');
 
-var actions = require('../actions');
 var SchemeStore = require('../stores/scheme');
 
 var Schemes = React.createClass({
 
-  handleExecute: function (file) {
-    actions.setAllColors(file.contents.colors);
-  },
-
   render: function () {
-    var schemes = Ranger.parseFiles(SchemeStore.getSchemes(), {
-      id: 'name'
-    });
-
     return (
       /* jshint ignore: start */
-      <Ranger
-        initialDir={schemes}
-        onExecute={this.handleExecute}
-      />
+      <Ranger store={SchemeStore.rangerStore()} />
       /* jshint ignore: end */
     );
   }
--- lib/components/templateBrowser.react.jsx
+++ lib/components/templateBrowser.react.jsx
@@ -1,43 +1,18 @@
 'use strict';
 
 var React = require('react');
-var Reflux = require('reflux');
 var Ranger = require('react-ranger');
 
 var TemplateStore = require('../stores/template');
-var actions = require('../actions');
 
 var TemplateBrowser = React.createClass({
-
-  mixins: [Reflux.ListenerMixin],
-
-  getInitialState: function () {
-    return {
-      templates: TemplateStore.availableTemplates()
-    };
-  },
-
-  componentDidMount: function () {
-    this.listenTo(TemplateStore, this._onChange);
-  },
-
-  handleExecute: function (item) {
-    actions.openTemplate(item.path);
-  },
 
   render: function () {
     return (
       /* jshint ignore: start */
-      <Ranger
-        initialDir={Ranger.parseList(this.state.templates)}
-        onExecute={this.handleExecute}
-      />
+      <Ranger store={TemplateStore.rangerStore()} />
       /* jshint ignore: end */
     );
-  },
-
-  _onChange: function () {
-    this.setState(this.getInitialState());
   }
 
 });
