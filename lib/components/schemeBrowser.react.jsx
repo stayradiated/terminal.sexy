60cc640aa3381fc4230ae994cdff460243a61220
George Czabania <george@czabania.com>
2014-09-03 22:21:30 +1200

    Display scheme preview in browser

--- lib/components/schemeBrowser.react.jsx
+++ lib/components/schemeBrowser.react.jsx
@@ -5,12 +5,48 @@ var Ranger = require('react-ranger');
 
 var SchemeStore = require('../stores/scheme');
 
+var ItemView = React.createClass({
+  render: function () {
+    var item = this.props.item.contents;
+
+    var colorSquares = [];
+    var background = item.colors.background.toHex();
+
+    for (var i = 0; i < 16; i += 1) {
+      var color = item.colors[i];
+      colorSquares.push(
+        /* jshint ignore: start */
+        <div key={i} style={{
+          background: color.toHex(),
+          borderColor: background
+        }} />
+        /* jshint ignore: end */
+      );
+    }
+
+    return (
+      /* jshint ignore: start */
+      <div className='schemes'>
+        <h1>{item.name}</h1>
+        <div>By: {item.author}</div>
+        <div className='colors' style={{ background: background }}>
+          {colorSquares}
+        </div>
+      </div>
+      /* jshint ignore: end */
+    );
+  }
+});
+
 var Schemes = React.createClass({
 
   render: function () {
     return (
       /* jshint ignore: start */
-      <Ranger store={SchemeStore.rangerStore()} />
+      <Ranger
+        store={SchemeStore.rangerStore()}
+        view={ItemView}
+      />
       /* jshint ignore: end */
     );
   }
