257e4637a037b632b2f902268f3f54f4577b6311
George Czabania <george@czabania.com>
2014-10-06 23:49:58 +1300

    Start working on saving schemes

---
+++ lib/components/save.react.jsx
@@ -0,0 +1,38 @@
+'use strict';
+
+var React = require('react');
+
+var actions = require('../actions');
+
+var Save = React.createClass({
+
+  handleSave: function () {
+    var path = this.refs.path.getDOMNode().value;
+    var author = this.refs.author.getDOMNode().value;
+    if (! path.length) { return; }
+    actions.saveScheme(path, author);
+  },
+
+  render: function () {
+    return (
+      /* jshint ignore: start */
+      <div className='save'>
+        <div className="control">
+          <label>Path:</label>
+          <input type='text' placeholder='/my-schemes/magical' ref='path' />
+        </div>
+        <div className="control">
+          <label>Author:</label>
+          <input type='text' placeholder='John Smith' ref='author' />
+        </div>
+        <div className='buttons'>
+          <button type='button' onClick={this.handleSave} className='button button-export'>Save</button>
+        </div>
+      </div>
+      /* jshint ignore: end */
+    );
+  },
+
+});
+
+module.exports = Save;
