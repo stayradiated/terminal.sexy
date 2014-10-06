b4ddd2aeecf59e40abd0c7ff25955b293c00c7cf
George Czabania <george@czabania.com>
2014-10-06 17:41:25 +1300

    Auto load schemes when browsing

--- lib/components/schemeBrowser.react.jsx
+++ lib/components/schemeBrowser.react.jsx
@@ -1,11 +1,25 @@
 'use strict';
 
 var React = require('react');
+var Reflux = require('reflux');
 var Ranger = require('react-ranger');
 
+var actions = require('../actions');
 var SchemeStore = require('../stores/scheme');
 
 var ItemView = React.createClass({
+
+  mixins: [Reflux.ListenerMixin],
+
+  componentDidMount: function () {
+    this.listenTo(SchemeStore, this._onChange);
+    actions.loadScheme(this.props.item);
+  },
+
+  componentWillReceiveProps: function (nextprops) {
+    actions.loadScheme(nextprops.item);
+  },
+
   render: function () {
     var item = this.props.item.contents;
 
@@ -17,7 +31,10 @@ var ItemView = React.createClass({
     var background = item.colors.background.toHex();
 
     for (var i = 0; i < 16; i += 1) {
-      var color = item.colors[i];
+      var id = Math.floor(i/2);
+      if (i % 2 === 1) { id += 8; }
+
+      var color = item.colors[id];
       colorSquares.push(
         /* jshint ignore: start */
         <div key={i} style={{
@@ -39,7 +56,12 @@ var ItemView = React.createClass({
       </div>
       /* jshint ignore: end */
     );
+  },
+
+  _onChange: function () {
+    this.forceUpdate();
   }
+
 });
 
 var Schemes = React.createClass({
