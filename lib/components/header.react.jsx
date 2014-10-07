3e87847ca49a6019c897c04e41e77cfce1521ce9
George Czabania <george@czabania.com>
2014-10-08 09:20:38 +1300

    Fix issue with not being able to create windows when no containers exist

--- lib/components/header.react.jsx
+++ lib/components/header.react.jsx
@@ -1,6 +1,6 @@
 'use strict';
 
-var $ = require('jquery');
+var _ = require('lodash');
 var React = require('react');
 var Reflux = require('reflux');
 
@@ -9,31 +9,6 @@ var WindowStore = require('../stores/window');
 
 var Header = React.createClass({
 
-  /*
-  mixins: [Reflux.ListenerMixin],
-
-  componentDidMount: function () {
-    this.listenTo(WindowStore, this.createDragSource);
-  },
-
-  createDragSource: function (layout) {
-    var $menu = $(this.refs.menu.getDOMNode());
-    $menu.find('li').each(function (_, el) {
-      var $el = $(el);
-      var id = $el.data('id');
-      layout.createDragSource($el, {
-        type: 'component',
-        componentName: id,
-        id: id,
-      });
-    });
-  },
-  */
-
-  open: function (id) {
-    actions.openWindow(id);
-  },
-
   render: function () {
     return (
       /* jshint ignore: start */
@@ -41,20 +16,31 @@ var Header = React.createClass({
         <h1 className='foreground-3'>terminal.sexy</h1>
         <div className='sections'>
           <div className='section'>
-            <span onClick={this.open.bind(this, 'about')}>About</span>
+            <span onClick={_.partial(actions.openWindow, 'about')}>About</span>
           </div>
           <div className='section'>
             <span>Open Window</span>
             <ul className='background-desktop' ref='menu'>
-              <li onClick={this.open.bind(this, 'editor')}>Editor</li>
-              <li onClick={this.open.bind(this, 'colorpicker')}>Color Picker</li>
-              <li onClick={this.open.bind(this, 'templates')}>Templates</li>
-              <li onClick={this.open.bind(this, 'schemes')}>Schemes</li>
-              <li onClick={this.open.bind(this, 'random')}>Randomiser</li>
-              <li onClick={this.open.bind(this, 'settings')}>Settings</li>
-              <li onClick={this.open.bind(this, 'import')}>Import Scheme</li>
-              <li onClick={this.open.bind(this, 'export')}>Export Scheme</li>
-              <li onClick={this.open.bind(this, 'importTemplate')}>Import Template</li>
+              <li onClick={_.partial(actions.openWindow, 'editor')}>Editor</li>
+              <li onClick={_.partial(actions.openWindow, 'colorpicker')}>Color Picker</li>
+              <hr className='background-subtle' />
+              <li onClick={_.partial(actions.openWindow, 'templates')}>Template Browser</li>
+              <li onClick={_.partial(actions.openWindow, 'schemes')}>Scheme Browser</li>
+              <hr className='background-subtle' />
+              <li onClick={_.partial(actions.openWindow, 'save')}>Save Scheme</li>
+              <li onClick={_.partial(actions.openWindow, 'import')}>Import Scheme</li>
+              <li onClick={_.partial(actions.openWindow, 'export')}>Export Scheme</li>
+              <hr className='background-subtle' />
+              <li onClick={_.partial(actions.openWindow, 'importTemplate')}>Import Template</li>
+              <hr className='background-subtle' />
+              <li onClick={_.partial(actions.openWindow, 'settings')}>Settings</li>
+              <li onClick={_.partial(actions.openWindow, 'random')}>Randomiser</li>
+            </ul>
+          </div>
+          <div className='section'>
+            <span>Workspace Layout</span>
+            <ul className='background-desktop' ref='menu'>
+              <li onClick={actions.resetLayout}>Reset Layout</li>
             </ul>
           </div>
         </div>
